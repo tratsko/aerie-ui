439db72e1a4660783a4b0db31bad1186ab8a3b32
psubram3 <pranav.subramanian@nasa.gov>
2025-07-23 12:49:36 -0700

    simplify logout

--- src/routes/oidc/logout/+server.ts
+++ src/routes/oidc/logout/+server.ts
@@ -2,27 +2,7 @@ import { redirect } from '@sveltejs/kit';
 
 export const GET = async ({ cookies }) => {
   console.debug('/auth/logout');
-  const refreshToken = cookies.get('refreshToken');
   const idToken = cookies.get('idToken') ?? '';
-
-  // revoke the refresh token (if present)
-  if (refreshToken) {
-    const refreshRevokeResponse = await fetch(
-      'https://keycloak.shared-services.appdat.jsc.nasa.gov/auth/realms/ssmo-dev/protocol/openid-connect/logout',
-      {
-        body: new URLSearchParams({
-          client_id: 'ssmo-dev-shared-aerie',
-          refresh_token: refreshToken,
-        }),
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        method: 'POST',
-      },
-    );
-
-    console.log(refreshRevokeResponse);
-  }
 
   // delete cookies here
   cookies.delete('accessToken', { path: '/' });
@@ -34,7 +14,7 @@ export const GET = async ({ cookies }) => {
     'https://keycloak.shared-services.appdat.jsc.nasa.gov/auth/realms/ssmo-dev/protocol/openid-connect/logout',
   );
 
-  keycloakLogoutUrl.searchParams.set('post_logout_redirect_uri', 'http://localhost:3000/plans'); // TODO: ??????? IS THIS RIGHT?
+  keycloakLogoutUrl.searchParams.set('post_logout_redirect_uri', 'http://localhost:3000/');
   keycloakLogoutUrl.searchParams.set('id_token_hint', idToken);
 
   // return a string for the redirect URL...
