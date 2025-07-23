73443e2572bbae630f6afda5c7303be129b21536
psubram3 <pranav.subramanian@nasa.gov>
2025-07-23 10:10:02 -0700

    minor fix for pathname exception

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,11 +1,14 @@
-// import { base } from '$app/paths';
-// import { redirect } from '@sveltejs/kit';
-// import { shouldRedirectToLogin } from '../utilities/login';
+import { base } from '$app/paths';
+import { env } from '$env/dynamic/public';
+import { redirect } from '@sveltejs/kit';
+import { shouldRedirectToLogin } from '../utilities/login';
 import type { LayoutServerLoad } from './$types';
 
 export const load: LayoutServerLoad = async ({ locals, url }) => {
-  // if (!url.pathname.includes('login') && shouldRedirectToLogin(locals.user)) { // TODO: we do need this because cam depends on it :(, wrap in an if not doing OIDC case
-  //   redirect(302, base);
-  // }
+  if (env.PUBLIC_AUTH_OIDC_ENABLED === 'false') {
+    if (!url.pathname.includes('login') && shouldRedirectToLogin(locals.user)) {
+      redirect(302, base);
+    }
+  }
   return { ...locals };
 };
