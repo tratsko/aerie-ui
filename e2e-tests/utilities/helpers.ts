f9de4a8055dfd1a77732e1e0ee3550e7fe4d2c42
psubram3 <pranav.subramanian@nasa.gov>
2025-07-24 11:46:26 -0700

    fix helper in tests with respect to token stuff

--- e2e-tests/utilities/helpers.ts
+++ e2e-tests/utilities/helpers.ts
@@ -1,6 +1,9 @@
 import { Cookie } from '@playwright/test';
 
 export function getUserCookieValue(cookies: Cookie[]): string | undefined {
+  if (process.env.PUBLIC_AUTH_OIDC_ENABLED === 'true') {
+    return cookies.find(cookie => cookie.name === 'accessToken')?.value;
+  }
   for (const cookie of cookies) {
     if (cookie.name === 'user') {
       return JSON.parse(atob(cookie.value)).token;
